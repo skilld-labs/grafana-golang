6941fd8db7d5ebd54ba7b3b1a8e9e042bcab5301
Michael Meunier <mmeunier@skilld.cloud>
2019-08-30 15:36:03 +0200

    First commit containing a big part of grafana API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-grafana
 Golang grafana API client
+
+Author: [Michael Meunier](https://github.com/mickymiek)
+Heavily inspired on [go-gitlab](https://github.com/xanzy/go-gitlab)
---
+++ dashboards_permissions.go
@@ -0,0 +1,63 @@
+package grafana
+
+import (
+	"fmt"
+	"time"
+)
+
+type DashboardsPermissionsService struct {
+	client *Client
+}
+
+type DashboardPermissions []DashboardPermission
+
+type DashboardPermission struct {
+	ID             int        `json:"id,omitempty"`
+	UID            string     `json:"uid,omitempty"`
+	DashboardID    int        `json:"folderId,omitempty"`
+	UserID         int        `json:"userId,omitempty"`
+	UserLogin      string     `json:"userLogin,omitempty"`
+	UserEmail      string     `json:"userEmail,omitempty"`
+	TeamID         int        `json:"teamId,omitempty"`
+	Team           string     `json:"team,omitempty"`
+	Role           string     `json:"role,omitempty"`
+	Permission     int        `json:"permission,omitempty"`
+	PermissionName string     `json:"permissionName,omitempty"`
+	Title          string     `json:"title,omitempty"`
+	Slug           string     `json:"slug,omitempty"`
+	IsFolder       bool       `json:"isFolder,omitempty"`
+	URL            string     `json:"url,omitempty"`
+	Created        *time.Time `json:"created,omitempty"`
+	Updated        *time.Time `json:"updated,omitempty"`
+}
+
+func (s *DashboardsPermissionsService) GetDashboardPermissions(id int) (*DashboardPermissions, *Response, error) {
+	u := fmt.Sprintf("dashboards/%d/permissions", id)
+
+	req, err := s.client.NewRequest("GET", u, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dp := new(DashboardPermissions)
+	resp, err := s.client.Do(req, dp)
+	if err != nil {
+		return nil, resp, err
+	}
+	return dp, resp, err
+}
+
+type UpdateDashboardPermissionsOptions struct {
+	Items DashboardPermissions `json:"items"`
+}
+
+func (s *DashboardsPermissionsService) UpdateDashboardPermissions(id int, opt *UpdateDashboardPermissionsOptions) (*Response, error) {
+	u := fmt.Sprintf("dashboards/%d/permissions", id)
+
+	req, err := s.client.NewRequest("POST", u, opt, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(req, nil)
+}
